dfaf765b64f90653d7303228932faa56c5b0593b
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-02-21 07:29:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-smhnef
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-smhnef)
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import react from 'react'
+
+<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+<div id="root"></div>
+
+<script type="text/babel">
+
+class PictureInPicture extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isPictureInPictureOpen: false
+    };
+  }
+
+  handlePictureInPictureClick = () => {
+    this.setState({
+      isPictureInPictureOpen: !this.state.isPictureInPictureOpen
+    });
+  };
+
+  render () {
+    return (
+      <div className="container">
+        <div className="content">
+          {!this.state.isPictureInPictureOpen && (
+            <img src="https://picsum.photos/200" />
+          )}
+        </div>
+        <button onClick={this.handlePictureInPictureClick}>
+          Start/ Stop Picture in Picture
+        </button>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<PictureInPicture />, document.getElementById('root'));
+
+</script>
